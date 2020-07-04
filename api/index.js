60a67b82cf91b91abd0852be53922244f5ad17c1
Carlos Navia T <cenaviat@gmail.com>
2020-07-04 00:28:40 -0500

    [ADD] auth

--- api/index.js
+++ api/index.js
@@ -4,8 +4,9 @@ const bodyParser = require('body-parser');
 const config = require('../config.js');
 const user = require('./components/users/network')
 const auth = require('./components/auth/network')
+const errors = require('../network/errors');
 
-const app =express();
+const app = express();
 
 app.use(bodyParser.json());
 
@@ -13,6 +14,8 @@ app.use(bodyParser.json());
 app.use('/api/user', user);
 app.use('/api/auth', auth);
 
-app.listen(config.api.port, ()=>{
+app.use(errors);
+
+app.listen(config.api.port, () => {
     console.log('Api escuchando en el puerto', config.api.port)
 })
--- auth/index.js
+++ auth/index.js
@@ -1,9 +1,52 @@
 const jwt = require('jsonwebtoken');
+const config = require('../config');
+const error = require('../utils/error');
+
+const secret = config.jwt.secret;
 
 function sign(data){
-    return jwt.sign(data, 'secreto');
+    return jwt.sign(data, secret);
+}
+
+function verify(token){
+    return jwt.verify(token, secret)
+}
+
+const check ={
+    own: function(req, owner){
+        const decoded = decodeHeader(req);
+        console.log(decoded);
+
+        if(decoded.id !== owner){
+            throw Error('La accion no es permitida para el usuario', 401);
+        }
+    },
+}
+
+function getToken(auth){
+    if(!auth){
+        throw new Error('No viene Token');
+    }
+
+    if (auth.indexOf('Bearer ') === -1){
+        throw new Error('Formato Invalido');
+    }
+    let token = auth.replace('Bearer ', '');
+
+    return token;
+}
+
+function decodeHeader(req){
+    const authorizacion = red.header.authorizacion || '';
+    const token = getToken(authorizacion);
+    const decoded = verify(token);
+
+    req.user = decoded;
+
+    return decoded;
 }
 
 module.exports = {
     sign,
+    check,
 };
